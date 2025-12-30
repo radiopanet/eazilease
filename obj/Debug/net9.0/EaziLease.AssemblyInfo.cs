ba7fa2f82376b21fa9ad9341a53565a49fb3ae13
radiopanet <radiopanethabo323@gmail.com>
2025-12-30 18:31:13 +0200

    Added new layout to super admin area

--- obj/Debug/net9.0/EaziLease.AssemblyInfo.cs
+++ obj/Debug/net9.0/EaziLease.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EaziLease")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ace488b8bb005b6d25653ad24acb00fef4fdbb2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d0970f2a290b33e18825ac70411693e98c23be7")]
 [assembly: System.Reflection.AssemblyProductAttribute("EaziLease")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EaziLease")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

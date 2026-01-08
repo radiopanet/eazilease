990c74115266b10af29ba88523083c85ff6e1205
radiopanet <radiopanethabo323@gmail.com>
2026-01-08 09:11:24 +0200

    Redesign suppliers views(create, edit,delete) and add details view

--- obj/Debug/net9.0/EaziLease.AssemblyInfo.cs
+++ obj/Debug/net9.0/EaziLease.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EaziLease")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+947336414c174418ab9688024d9520aa259730b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+939439d5de81d618a998dc470150ee898f75107c")]
 [assembly: System.Reflection.AssemblyProductAttribute("EaziLease")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EaziLease")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

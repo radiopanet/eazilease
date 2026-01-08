e341e3106991ef1981e60e03a95576de6b58f443
radiopanet <radiopanethabo323@gmail.com>
2026-01-08 16:57:42 +0200

    SuperAdmin Layout Change

--- obj/Debug/net9.0/EaziLease.AssemblyInfo.cs
+++ obj/Debug/net9.0/EaziLease.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EaziLease")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+045b9f65ce23bcb7b9d1a9e9fbd21942aa9c1bb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b67759ef5acd9fd3d8443b1cae87b5cdc2646b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("EaziLease")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EaziLease")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

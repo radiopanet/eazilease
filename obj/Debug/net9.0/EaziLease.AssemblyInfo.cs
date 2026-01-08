64209363aae3b73489836811c48407274cc8d98e
radiopanet <radiopanethabo323@gmail.com>
2026-01-08 11:36:35 +0200

    Fix odometer reading update

--- obj/Debug/net9.0/EaziLease.AssemblyInfo.cs
+++ obj/Debug/net9.0/EaziLease.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EaziLease")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+426b009b479e72e56dd16c4d6b9d38afd9e32f81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2610ee947b2eeeb7c6fe8973f9068d8605aee52")]
 [assembly: System.Reflection.AssemblyProductAttribute("EaziLease")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EaziLease")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

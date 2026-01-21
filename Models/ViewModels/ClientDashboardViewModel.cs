fbb97873e9a35d368645ff38665b8b6e98431102
radiopanet <radiopanethabo323@gmail.com>
2026-01-21 19:40:29 +0200

    Added Login and ClientDashboardViewModel

---
+++ Models/ViewModels/ClientDashboardViewModel.cs
@@ -0,0 +1,11 @@
+namespace EaziLease.Models.ViewModels
+{
+    public class ClientDashboardViewModel
+    {
+        public string CompanyName {get; set;} = string.Empty;
+        public int ActiveLeasesCount {get; set;}
+        public int TotalVehiclesLeased {get; set;}
+        public List<VehicleMaintenance>? UpcomingMaintenance {get; set;}
+        public decimal NextInvoiceAmount {get; set;}
+    }
+}
--- obj/Debug/net9.0/EaziLease.AssemblyInfo.cs
+++ obj/Debug/net9.0/EaziLease.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EaziLease")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6bfe7eb8db31f7aa4f9d3e70daac8406a79c2ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fcd94369d546653b4caecbd48a76c403218f8a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("EaziLease")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EaziLease")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
